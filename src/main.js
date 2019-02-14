e94baeac203912e73b29de305f88992e9baf2b48
K. Nattawut <kitiwan_n@silpakorn.edu>
2019-02-15 03:47:19 +0700

    Test

--- src/main.js
+++ src/main.js
@@ -11,8 +11,10 @@ import VueSweetalert2 from 'vue-sweetalert2';
 
 import 'semantic-ui-css/semantic.min.css';
 import "./assets/customize.css";
+import VueScroller from 'vue-scroller'
 
 Vue.config.productionTip = false
+Vue.use(VueScroller)
 Vue.use(SuiVue);
 Vue.use(VueSweetalert2);
 moment.locale("th");
@@ -40,32 +42,58 @@ new Vue({
       setTimeout(() => {
         store.commit("setLoading", false);
       }, 30 * 1000);
-
-      self.$liff.init(() => {
-        self.$liff.getProfile().then(async profile => {
-          store.commit("setAppId", APP_ID);
-          store.commit("setApiKey", KEY);
-          const appData = await self.$api.get(`/app/enterprise/${APP_ID}/${KEY}`);
-          if (appData) {
-            store.commit("setAppData", appData.data);
+      // if (!webpackHotUpdate) {
+      //   self.$liff.init(() => {
+      //     self.$liff.getProfile().then(async profile => {
+      //       store.commit("setAppId", APP_ID);
+      //       store.commit("setApiKey", KEY);
+      //       const appData = await self.$api.get(`/app/enterprise/${APP_ID}/${KEY}`);
+      //       if (appData) {
+      //         store.commit("setAppData", appData.data);
+      //         const user = await self.$api.get(`/app/employee/${profile.userId}/${APP_ID}`);
+      //         if (user.data) {
+      //           store.commit('setLiff', true)
+      //           store.commit("setUser", user.data);
+      //         } else {
+      //           const createUser = await self.$api.post(`/app/employee/${profile.userId}/${APP_ID}`,
+      //             profile
+      //           );
+      //           if (createUser.data) {
+      //             const user = await self.$api.get(`/app/employee/${profile.userId}/${APP_ID}`);
+      //             store.commit("setUser", user.data);
+      //           }
+      //         }
+      //       }
+      //       store.commit("setLoading", false);
+      //     })
+      //   })
+      // } else {
+      let profile = {
+        userId: "U983b8d629402fc43cd62e951978d6032",
+        displayName: ".ta",
+        pictureUrl: "https://i.stack.imgur.com/lcRsd.gif?s=32&g=1"
+      };
+      store.commit("setAppId", APP_ID);
+      store.commit("setApiKey", KEY);
+      const appData = await self.$api.get(`/app/enterprise/${APP_ID}/${KEY}`);
+      if (appData) {
+        store.commit("setAppData", appData.data);
+        const user = await self.$api.get(`/app/employee/${profile.userId}/${APP_ID}`);
+        if (user.data) {
+          // store.commit('setLiff', true)
+          store.commit("setUser", user.data);
+        } else {
+          const createUser = await self.$api.post(`/app/employee/${profile.userId}/${APP_ID}`,
+            profile
+          );
+          if (createUser.data) {
             const user = await self.$api.get(`/app/employee/${profile.userId}/${APP_ID}`);
-            if (user.data) {
-              store.commit('setLiff', true)
-              store.commit("setUser", user.data);
-            } else {
-              const createUser = await self.$api.post(`/app/employee/${profile.userId}/${APP_ID}`,
-                profile
-              );
-              if (createUser.data) {
-                const user = await self.$api.get(`/app/employee/${profile.userId}/${APP_ID}`);
-                store.commit("setUser", user.data);
-              }
-            }
+            store.commit("setUser", user.data);
           }
-          store.commit("setLoading", false);
-        })
-      })
-
+        }
+      }
+      //   store.commit("setLoading", false);
+      // }
 
     } else {
       store.commit("setLoading", false);
